cb7ade8722a53e1e7660fd4c6677471f900e6da5
ddkohler <ddkohler@users.noreply.github.com>
2021-03-29 15:41:15 -0500

    features tabulated

--- yaqd_andor/_neo_triggered.py
+++ yaqd_andor/_neo_triggered.py
@@ -1,11 +1,14 @@
 __all__ = ["NeoTriggered"]
 
 import asyncio
+from re import L
 import numpy as np
 
 from yaqd_core import IsDaemon, IsSensor, HasMeasureTrigger, HasMapping
 from typing import Dict, Any, List
 from . import atcore 
+from . import features
+from collections import namedtuple
 
 import os
 os.chdir(os.path.dirname(__file__))
@@ -19,12 +22,13 @@ class NeoTriggered(HasMapping, HasMeasureTrigger, IsSensor, IsDaemon):
 
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
-        print("Intialising SDK3")
-        import os
-        print(os.getcwd())
         self.sdk3 = ATCore() # Initialise SDK3
+        # find devices
         device_count = self.sdk3.get_int(self.sdk3.AT_HNDL_SYSTEM, "DeviceCount")
+        if device_count == 0:
+            raise ConnectionError("No devices found.")
         i = 0
+        # select device
         while i < device_count:
             temp = self.sdk3.open(i)
             serial = self.sdk3.get_string(temp, "SerialNumber")
@@ -32,31 +36,38 @@ class NeoTriggered(HasMapping, HasMeasureTrigger, IsSensor, IsDaemon):
                 self.hndl = temp
                 print("    Serial No   : ",serial)
                 break
+            i += 1
         else:
-            print("no devices found")
-        
+            raise ConnectionError(
+                r"device with serial number {0} not found".format(self._config["serial_number"])
+            )
 
-        self.hndl = self.sdk3.open(0)
-        if False:
+        if not self._config["is_virtual"]:
             self.sdk3.set_enumerated_string(
                 self.hndl,
                 "SimplePreAmpGainControl",
                 "16-bit (low noise & high well capacity)"
             )
-            # set trigger mode to software
-            self.sdk3.set_enumerated_string(
-                self.hndl, "TriggerMode", "Software"
-            )
-            # set cycle mode to fixed
-            self.sdk3.set_enumerated_string(
-                self.hndl, "CycleMode", "Fixed"
-            )
-            # enable metadata
+
+        # set trigger mode to software
+        self.sdk3.set_enumerated_string(
+            self.hndl, "TriggerMode", "Advanced" if self._config["is_virtual"] else "Software"
+        )
+        # set cycle mode to fixed
+        self.sdk3.set_enumerated_string(
+            self.hndl, "CycleMode", "Fixed"
+        )
+        # enable metadata
+        self.sdk3.set_enumerated_string(
+            self.hndl, "CycleMode", "Fixed"
+        )
+        self.sdk3.set_bool(self.hndl, "MetadataEnable", True)
 
     async def _measure(self):
         # queue buffer
         imageSizeBytes = self.sdk3.get_int(self.hndl, "ImageSizeBytes")
         buf = np.empty((imageSizeBytes,), dtype='B')
+        # todo: queue buffer for n frames
         self.sdk3.queue_buffer(self.hndl, buf.ctypes.data, imageSizeBytes)
         # acquire frame
         # self.sdk3.command(self.hndl, "AcquisitionStart")
@@ -70,7 +81,37 @@ class NeoTriggered(HasMapping, HasMeasureTrigger, IsSensor, IsDaemon):
 
         return {"image": pixels}
 
+    def get_sensor_info(self):
+        pass
+
+    def interrupt(self):
+        """stop measure before exposure time is reached.
+        """
+        pass
+
     def close(self):
         self.sdk3.close(self.hndl)
-        # ddk: need to call sdk3.__del__ ever?
+
+    def _get_feature(self, feature):
+        call = type_to_call["int"].format("get")
+        if self.sdk3.is_implemented(self.hndl, call):
+            if self.sdk3.is_readable(self.hndl, call):
+                return self.sdk3.__getattribute__(call)(self.hndl, feature.name)
+            else:
+                raise TypeError(f"call {call} is not readable")
+        else:
+            raise ValueError(f"call {call} is not implemented")
+
+    def _set_feature(self, feature):
+        call = type_to_call["int"].format("set")
+        if self.sdk3.is_readonly(self.hndl, call):
+            return TypeError(f"Cannot write.  Feature {call} is read only")
+        if self.sdk3.is_implemented(self.hndl, call):
+            while not self.sdk3.is_writable(self.hndl, call):
+                return self.sdk3.__getattribute__(call)(self.hndl, feature.name, feature.value)
+        else:
+            raise ValueError(f"call {call} is not implemented")
+
+
+
 
---
+++ yaqd_andor/features.py
@@ -0,0 +1,181 @@
+from collections import namedtuple
+
+FeatureSpec = namedtuple("Feature", ["sdk_name", "type", "availability"])
+
+# adapted from sdk3 manual
+# note some table entries were omitted (focused on neo, simcam features)
+# a = apogee, n = neo, s = simcam, z = zyla
+feature_specs = dict(
+    acquisition_start = FeatureSpec("AcquisitionStart", "command", "nsz"),
+    acquisition_stop = FeatureSpec("AcquisitionStop", "command", "nsz"),
+    aoi_binning = FeatureSpec("AOIBinning", "enumerated", "nz"),
+    aoi_hbin = FeatureSpec("AOIHBin", "integer", "asz"),
+    aoi_height = FeatureSpec("AOIHeight", "integer", "ansz"),
+    aoi_layout = FeatureSpec("AOILayout", "enumerated", "az"),
+    aoi_left = FeatureSpec("AOILeft", "integer", "ansz"),
+    aoi_stride = FeatureSpec("AOIStride", "integer", "nz"),
+    aoi_top = FeatureSpec("AOITop", "integer", "ansz"),
+    aoi_vbin = FeatureSpec("AOIVBin", "integer", "asz"),
+    aoi_width = FeatureSpec("AOIWidth", "integer", "ansz"),
+    baseline = FeatureSpec("Baseline", "integer", "nz"),
+    bit_depth = FeatureSpec("BitDepth", "enumerated", "anz"),
+    buffer_overflow_event = FeatureSpec("BufferOverflowEvent", "integer", "nz"),
+    bytes_per_pixel = FeatureSpec("BytesPerPixel", "float", "nz"),
+    camera_acquiring = FeatureSpec("CameraAcquiring", "boolean", "nsz"),
+    camera_dump = FeatureSpec("CameraDump", "command", "nz"),
+    camera_family = FeatureSpec("CameraFamily", "string", "a"),
+    camera_memory = FeatureSpec("CameraMemory", "integer", "a"),
+    camera_model = FeatureSpec("CameraModel", "string", "nsz"),
+    camera_name = FeatureSpec("CameraName", "string", "anz"),
+    cycle_mode = FeatureSpec("CycleMode", "enumerated", "ns"),
+    electronic_shuttering_mode = FeatureSpec("ElectronicShutteringMode", "enumerated", "nsz"),
+    event_enable = FeatureSpec("EventEnable", "boolean", "nz"),
+    events_missed_event = FeatureSpec("EventsMissedEvent", "integer", "nz"),
+    event_selector = FeatureSpec("EventSelector", "enumerated", "nz"),
+    exposure_time = FeatureSpec("ExposureTime", "float", "nsz"),
+    exposure_end_event = FeatureSpec("ExposureEndEvent", "integer", "nz"),
+    exposure_start_event = FeatureSpec("ExposureStartEvent", "integer", "nz"),
+    external_trigger_delay = FeatureSpec("ExposureTriggerDelay", "float", "z"),
+    # fan_speed = FeatureSpec("FanSpeed", "enumerated", "ansz"),
+    # fast_aoi_frame_rate_enable = FeatureSpec("FastAOIFrameRateEnable", "boolean", "nz"),
+    firmware_version = FeatureSpec("FirmwareVersion", "string", "anz"),
+    frame_count = FeatureSpec("FrameCount", "integer", "ansz"),
+    frame_rate = FeatureSpec("FrameInterval", "float", "ansz"),
+    full_aoi_control = FeatureSpec("FullAOIControl", "boolean", "nz"),
+    image_size_bytes = FeatureSpec("ImageSizeBytes", "integer", "nsz"),
+    interface_type = FeatureSpec("InterfaceType", "string", "anz"),
+    max_interface_transfer_rate = FeatureSpec("MaxInterfaceTransferRate", "float", "nz"),
+    metadata_enable = FeatureSpec("MetadataEnable", "boolean", "nz"),
+    metadata_frame = FeatureSpec("MetadataFrame", "boolean", "nz"),
+    metadata_timestamp = FeatureSpec("MetadataTimestamp", "boolean", "nz"),
+    overlap = FeatureSpec("Overlap", "boolean", "anz"),
+    pixel_correction = FeatureSpec("PixelCorrection", "enumerated", "s"),
+    pixel_encoding = FeatureSpec("PixelEncoding", "enumerated", "nsz"),
+    pixel_height = FeatureSpec("PixelHeight", "enumerated", "nsz"),
+    pixel_readout_rate = FeatureSpec("PixelReadoutRate", "enumerated", "ansz"),
+    pixel_width = FeatureSpec("PixelWidth", "float", "ansz"),
+    readout_time = FeatureSpec("ReadoutTime", "float", "nz"),
+    sensor_cooling = FeatureSpec("SensorCooling", "boolean", "ansz"),
+    sensor_height = FeatureSpec("SensorHeight", "integer", "ansz"),
+    sensor_readout_mode = FeatureSpec("SensorReadoutMode", "enumerated", "z"),
+    sensor_temperature = FeatureSpec("SensorTemperature", "float", "ansz"),
+    sensor_width = FeatureSpec("SensorWidth", "integer", "ansz"),
+    serial_number = FeatureSpec("SerialNumber", "string", "snz"),
+    simple_preamp_gain_control = FeatureSpec("SimplePreampGainControl", "enumerated", "nz"),
+    software_trigger = FeatureSpec("SoftwareTrigger", "command", "nz"),
+    temperature_control = FeatureSpec("TemperatureControl", "enumerated", "n"),
+    temperature_status = FeatureSpec("TemperatureStatus", "enumerated", "anz"),
+    timestamp_clock = FeatureSpec("TimestampClock", "integer", "nz"),
+    timestamp_clock_frequency = FeatureSpec("TimestampClockFrequency", "integer", "nz"),
+    timestamp_clock_reset = FeatureSpec("TimestampClockReset", "command", "nz"),
+    trigger_mode = FeatureSpec("TriggerMode", "enumerated", "ansz"),
+    vertically_center_aoi = FeatureSpec("VerticallyCentreAOI", "boolean", "nz")
+)
+
+shared_features = [k for k,v in feature_specs.items() if \
+    ("n" in v.availability) and ("s" in v.availability) and ("z" in v.availability)]
+simcam_features = [k for k,v in feature_specs.items() if "s" in v.availability]
+neo_features = [k for k,v in feature_specs.items() if "n" in v.availability]
+
+# print(simcam_features)
+# print(neo_features)
+# print(shared_features)
+
+# int - set, get, max, min
+# float - set, get, max, min
+# bool - set, get
+# enum - set str/idx, get str/idx
+# string - set, get, max length
+# command - command
+
+class Feature:
+    def __init__(self, sdk, hndl, spec):
+        """
+        command:  feature()
+        get: feature
+        set: feature.set(value)
+        """
+        self.type = spec.type
+        self.sdk_name = spec.sdk_name
+        self.sdk = sdk
+        self.hndl = hndl
+        self.method = r"{}" + f"_{type}"
+        self.is_implemented = self.sdk.is_implemented(self.hndl, self.sdk_name)
+        # implemented?
+        if self.is_implemented:
+                self.__repr__ = self._get
+        else:
+            raise NotImplementedError(f"feature {self.sdk_name} is not implemented")
+        self.is_readonly = self.sdk.is_readonly(self.hndl, self.sdk_name)
+        if not self.is_readonly:
+            self.set = self._set
+            self._set_call = f"set_{self.type}"
+        # is read only?
+        self._get_call = f"get_{self.type}"
+
+    def _get(self):
+        call = self._get_call
+        if self.sdk.is_readable(self.hndl, call):
+            return self.sdk.__getattribute__(call)(self.hndl, self.sdk_name)
+        else:
+            raise TypeError(f"call {call} is not readable")
+
+    def _set(self, value):
+        call = self._set_call
+        if self.sdk.is_writable(self.hndl, call):
+            return self.sdk.__getattribute__(call)(self.hndl, self.sdk_name, value)
+        else:
+            raise ValueError(f"call {call} is not implemented")    
+
+
+class String(Feature):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class Float(Feature):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def max(self) -> float:
+        pass
+
+    def min(self) -> float:
+        pass
+
+
+class Int(Feature):
+    def __init__(self, *args):
+        super().__init__(*args)
+        
+    def max(self) -> int:
+        pass
+
+    def min(self) -> int:
+        pass
+
+
+class Bool(Feature):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class Command(Feature):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        del self._set
+        del self._get
+
+    def __call__(self):
+        return self.sdk.command(self.hndl, self.sdk_name)
+
+
+class Enumerated(Feature):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self._get_call = self._get_call + "_string"
+        self._set_call = self._set_call + "_string"
+
+    def symbols(self):
+        return self.sdk.get_enumerated_string_options(self.hndl, self.sdk_name)
+

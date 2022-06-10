2589257d523e3a0224502c9a112fa88999f30465
unknown <kentmeyer@wisc.edu>
2022-06-10 10:55:01 -0500

    added methods, rerendered avpr, fixed typehint

--- yaqd_andor/_andorsdk2_ixon.py
+++ yaqd_andor/_andorsdk2_ixon.py
@@ -16,7 +16,7 @@ class AndorSdk2Ixon(_andor_sdk2.AndorSDK2):
         super().__init__(name, config, config_filepath)
         self._channel_names = ["image"]
         self._channel_units = {"image": "counts"}
-
+        self.stop_update==True
         # find devices
         code, device_count = self.sdk.GetAvailableCameras()
         if device_count == 0:
@@ -44,13 +44,13 @@ class AndorSdk2Ixon(_andor_sdk2.AndorSDK2):
         self.preamp_gain=int(self._config["simple_preamp_gain_control"])
         self.exposure_time=float(self._config["exposure_time"])
         self.has_mono=bool(self._config["has_monochromator"])
-        xai,yai,bin=self._set_aoi()
-        self._gen_mappings(xai, yai, bin)
+        self._set_aoi()
+        self._gen_mappings()
 
         # implement config, state features
         # see p97 of SDK2 on the conversion of noise filters from sdk3 to sdk2 or v-v.
         # Commented out until further work done to establish differences between
-        # these filters and SDK3.
+        # these filters and SDK3.  These may be hardcoded here rather than in config.
         #self.sdk.Filter_SetMode(int(self._config["spurious_noise_filter"]))
         #self.sdk.Filter_SetThreshold(float(self._config["filter_threshold"]))
         self.sdk.SetPreAmpGain(self.preamp_gain)
@@ -66,9 +66,9 @@ class AndorSdk2Ixon(_andor_sdk2.AndorSDK2):
 
         self._set_temperature()
         self.sdk.SetShutter(int(0),int(1),int(100),int(100))
+        self.stop_update=False
 
-
-    def _gen_mappings(self, x_ai, y_ai, binning):
+    def _gen_mappings(self):
         """Get map."""
         if self.has_mono:
             # translate inputs into appropriate internal units
@@ -80,8 +80,8 @@ class AndorSdk2Ixon(_andor_sdk2.AndorSDK2):
             self.spec_position = self._config["spectrometer_position"]
 
             # create array
-            i_pixel = np.array(x_ai, dtype=float)
-            eff_pixel_width_mm=float(int(binning)*pixel_width_mm)
+            i_pixel = np.array(self.x_ai, dtype=float)
+            eff_pixel_width_mm=float(int(self.binning)*pixel_width_mm)
             # calculate terms
             x = np.arcsin(
                 (1e-6 * self._config["order"] * self._config["grooves_per_mm"] * self.spec_position)
@@ -101,11 +101,11 @@ class AndorSdk2Ixon(_andor_sdk2.AndorSDK2):
                 )
             )
             out = ((A + B) * float(1e6)) / (float(self._config["order"]) * float(self._config["grooves_per_mm"]))
-            self._mappings= {"x_index": out, "y_index": y_ai}
+            self._mappings= {"x_index": out, "y_index": self.y_ai}
         else:
             self._channel_mappings = {"image": ["x_index", "y_index"]}
             self._mapping_units = {"x_index": "None", "y_index": "None"}
-            self._mappings = {"x_index": x_ai, "y_index": y_ai}
+            self._mappings = {"x_index": self.x_ai, "y_index": self.y_ai}
         return out
 
 
@@ -146,19 +146,20 @@ class AndorSdk2Ixon(_andor_sdk2.AndorSDK2):
         self.logger.info(f"binning={binning}, left_aoi=pixel {left}, right_aoi=pixel {right}, top_aoi=pixel {top}, bottom_aoi=pixel {bottom}.")
         if code != 20002:
             raise ValueError(str(self.errorlookup(code)))
+            return code
+        else:
+            self.buffer_size=int(int(width)*int(height))
+            self.buffer=np.zeros([arrwidth, arrheight], dtype=int)
+            self.buffer=np.ascontiguousarray(self.buffer, dtype=int)
 
-        self.buffer_size=int(int(width)*int(height))
-        self.buffer=np.zeros([arrwidth, arrheight], dtype=int)
-        self.buffer=np.ascontiguousarray(self.buffer, dtype=int)
-
-        # apply shape, mapping
-        self._channel_shapes = {
-            "image": (int(height), int(width))
-        }
-        x_ai = np.arange(int(left), int(left + width * binning), int(binning), dtype=int)[None, :]
-        y_ai = np.arange(int(top), int(top + height * binning), int(binning), dtype=int)[:, None]
-
-        return x_ai, y_ai, binning
+            # apply shape, mapping
+            self._channel_shapes = {
+                "image": (int(height), int(width))
+            }
+            self.x_ai = np.arange(int(left), int(left + width * binning), int(binning), dtype=int)[None, :]
+            self.y_ai = np.arange(int(top), int(top + height * binning), int(binning), dtype=int)[:, None]
+            self.binning= int(binning)
+            return 0
 
 
     def _set_temperature(self):
@@ -192,11 +193,59 @@ class AndorSdk2Ixon(_andor_sdk2.AndorSDK2):
         self.logger.info("Sensor temp is stabilized.")
 
 
-    def close(self):
-        #stop loop
+    def set_spec_position(self, spec_position):
+        # Sets the CCD mapping to a new spectrometer position (nm)
+        if self.has_mono:
+            self.spec_position=float(spec_position)
+            self._gen_mappings()
+            self.logger.info(f"new spectrometer position: {self.spec_position} nm")
+        else:
+            self.logger.info(f"Attempt to change spectrometer position when monochromator portion not enabled.")
+
+
+    def get_spec_position(self):
+        # Gets the CCD mapping to the spectrometer position (nm)
+        if self.has_mono:
+            return self.spec_position
+        else:
+            self.logger.info(f"Attempt to change spectrometer position when monochromator portion not enabled.")
+            return float(0.00)
+
+
+    def set_exposure_time(self, exposure_time):
+        # Sets the exposure time in seconds (float)
+        self.stop_update=True
         while self._busy==True:
             sleep(0.10)
+        code=self.sdk.SetExposureTime(float(exposure_time))
+        if code != 20002:
+            raise ValueError(str(self.errorlookup(code)))
+        else:
+            self.exposure_time=exposure_time
+            self.logger.info(f"New exposure time is {self.exposure_time} sec.")
+        self.stop_update=False
+        return code
+
+
+    def get_exposure_time(self):
+        # Gets the exposure time in seconds (float)
+        return self.exposure_time
+
+
+    def get_spec_position(self):
+        # Gets the CCD mapping to the spectrometer position (nm)
+        if self.has_mono:
+            return self.spec_position
+        else:
+            self.logger.info(f"Attempt to change spectrometer position when monochromator portion not enabled.")
+            return float(0.00)
+
+
+    def close(self):
+        #stop loop
         self.stop_update=True
+        while self._busy==True:
+            sleep(0.10)
         self.sdk.SetShutter(int(0),int(2),int(100),int(100))
         sleep(0.50)
         self.sdk.CoolerOFF()
